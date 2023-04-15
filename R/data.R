282493807bf84769296c13f081a686d0e5d83b76
AndreaRaisman <raismanandrea@gmail.com>
2023-04-15 12:23:30 +0000

    New codon_table object with description

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' codon_table
+#'
+#' This is a table with the DNA codes for the 20 amino acids. There are 20 letters, 1 per amino acid and 64 possible nucleotide combinations.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+
+"codon_table"
