5f914a7e2dddacd96a5a419a58f093509a886167
AndreaRaisman <raismanandrea@gmail.com>
2023-04-16 09:53:27 +0000

    Added function to create graph that counts amino acid occurrences

---
+++ R/aa_graph.R
@@ -0,0 +1,25 @@
+#' aa_graph
+#'
+#' @param aa_seq amino acid sequence
+#'
+#' @return aa_plot plot of the amino acid frequencies
+#' @export
+#'
+#' @examples If you input LLNRKKK, L is 2/7; N is 1/7, R is 1/7 and K is 3/7
+aa_graph <- function(aa_seq){
+  aa_string <- aa_seq %>%
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) %>%
+    as.character() %>%
+    unique()
+  counts <- sapply(aa_string, function(amino_acid) stringr::str_count(string = aa_seq, pattern =
+                                                                       amino_acid)) %>%
+    as.data.frame()
+  colnames(counts) <- c("Counts")
+  counts[["AA_seq"]] <- rownames(counts)
+  aa_plot <- counts %>%
+    ggplot2::ggplot(ggplot2::aes(x = AA_seq, y = Counts, fill = AA_seq)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+  return(aa_plot)
+}
